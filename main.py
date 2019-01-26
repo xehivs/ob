d0b8dfdf28a0beae9f65e7a87e0667db8e01a159
matbur <mateusz@burniak.pl>
2019-01-26 22:21:03 +0100

    Add canny

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import requests
+from skimage.feature import canny
+import cv2
 
 
 DATA = Path('./data/')
@@ -116,18 +118,27 @@ def main():
 
     df = pd.DataFrame(X_pca[:, :2])
     df = pd.concat([df, y, colors], axis=1)
-    df.columns = ['PCA1', 'PCA2', 'target', 'color']
+    df.columns = ['PC1', 'PC2', 'target', 'color']
 
-    df = df[df['target'] != 0]
+    df_0 = df[df['target'] != 0]
 
     fig, ax = plt.subplots(1, 1)
     ax.set_xlabel('PC-1')
     ax.set_ylabel('PC-2')
     ax.set_title('PCA on Indian Pines dataset')
     ax.grid()
-    ax.scatter(df['PCA1'], df['PCA2'], color=df['color'], s=3)
+    ax.scatter(df_0['PC1'], df_0['PC2'], color=df_0['color'], s=3)
     fig.savefig(IMG / 'pc1_pc2.png')
 
+    img = (df['PC1'] + df['PC2']).values.reshape((145, 145))
+    plt.imsave(IMG / 'pc12.png', img)
+
+    c = canny(img, sigma=2., low_threshold=.15,
+              high_threshold=.6, use_quantiles=True)
+    plt.imsave(IMG / 'pc12_canny.png', c)
+
+    gt2 = cv2.imread((IMG / 'gt.png').as_posix(), 0)
+    plt.imsave(IMG / 'gt_canny.png', canny(gt2))
 
 if __name__ == '__main__':
     main()
